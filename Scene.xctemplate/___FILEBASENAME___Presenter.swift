264e44f25b7d0dcf4d88179f189f728bc4d7859b
Diego Trevisan Lara <diego@trevisa.nl>
2018-02-22 17:13:27 -0300

    Added templates for the Scene concept.

---
+++ Scene.xctemplate/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,23 @@
+//___FILEHEADER___
+
+import UIKit
+
+protocol I___VARIABLE_sceneName:identifier___View: class {
+    
+}
+
+protocol I___VARIABLE_sceneName:identifier___Presenter {
+    
+}
+
+class ___VARIABLE_sceneName:identifier___Presenter: I___VARIABLE_sceneName:identifier___Presenter {
+    
+    weak var view: I___VARIABLE_sceneName:identifier___View?
+    let router: I___VARIABLE_sceneName:identifier___ViewRouter
+    
+    init(view: I___VARIABLE_sceneName:identifier___View, router: I___VARIABLE_sceneName:identifier___ViewRouter) {
+        self.view = view
+        self.router = router
+    }
+    
+}
