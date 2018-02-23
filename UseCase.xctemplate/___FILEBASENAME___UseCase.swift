a11ae5006b3de8765cd4a3bbd68347df533c73a6
Diego Trevisan Lara <diego@trevisa.nl>
2018-02-23 09:57:50 -0300

    Added UseCase template.

---
+++ UseCase.xctemplate/___FILEBASENAME___UseCase.swift
@@ -0,0 +1,21 @@
+//___FILEHEADER___
+
+import UIKit
+
+protocol I___VARIABLE_sceneName:identifier___UseCase {
+//    func get(parameters: ParametersObject, completion: @escaping (_ customer: Result<Object>) -> Void)
+}
+
+class ___VARIABLE_sceneName:identifier___UseCase: I___VARIABLE_sceneName:identifier___UseCase {
+    
+//    private let myGateway: MyGateway
+//
+//    init(myGateway: MyGateway) {
+//        self.myGateway = myGateway
+//    }
+//
+//    func get(parameters: ParametersObject, completion: @escaping (Result<Object>) -> Void) {
+//        myGateway.get(parameters: parameters, completion: completion)
+//    }
+    
+}
