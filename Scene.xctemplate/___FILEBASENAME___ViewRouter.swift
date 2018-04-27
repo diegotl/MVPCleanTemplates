f5995e9f3ee3d017b44262595544e942ee0af8c9
Diego Trevisan Lara <diego@trevisa.nl>
2018-04-27 17:40:37 -0300

    Removed router's viewController reference unwrapping

--- Scene.xctemplate/___FILEBASENAME___ViewRouter.swift
+++ Scene.xctemplate/___FILEBASENAME___ViewRouter.swift
@@ -3,15 +3,15 @@
 import UIKit
 
 protocol I___VARIABLE_sceneName:identifier___ViewRouter {
-    
+
 }
 
 class ___VARIABLE_sceneName:identifier___ViewRouter: I___VARIABLE_sceneName:identifier___ViewRouter {
-    
-    private weak var viewController: ___VARIABLE_sceneName:identifier___ViewController!
-    
+
+    private weak var viewController: ___VARIABLE_sceneName:identifier___ViewController?
+
     init(viewController: ___VARIABLE_sceneName:identifier___ViewController) {
         self.viewController = viewController
     }
-    
+
 }
